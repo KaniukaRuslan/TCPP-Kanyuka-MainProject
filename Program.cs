6d8bc229602474e15290937c79111ec574d07ab6
Kaniuka Ruslan <kaniuka.r@donnu.edu.ua>
2021-04-22 03:42:26 +0300

    ChangesALFA Зміни_1

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace TCPP_Project
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Змінни в ALFA");
         }
     }
 }
